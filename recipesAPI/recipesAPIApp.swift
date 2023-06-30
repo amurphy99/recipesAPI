086c64fdb5d8c73764645c3327f4cbeb82b85f60
amurphy99 <amurphy62299@gmail.com>
2023-06-30 14:16:29 -0500

    Initial Commit

---
+++ recipesAPI/recipesAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  recipesAPIApp.swift
+//  recipesAPI
+//
+//  Created by Andrew Murphy on 6/30/23.
+//
+
+import SwiftUI
+
+@main
+struct recipesAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
